478c24262f7a58e11b000cbb9ee5b6201ee73cf6
sukhvirdi7 <sukh.virdi2@mhra.gov.uk>
2020-09-22 13:17:38 +0100

    script changes

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById
+('question-container')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+const shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
-
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function setNextQuestion() {
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function setAnswer() {
     
 }
+
+const questions = [
+    {
+      questions: 'What is 2 + 2',
+      answers: [
+       { text: '4', correct: true},
+       { text: '22', correct: true},
+    ]
+}
+]
